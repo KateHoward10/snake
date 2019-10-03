091f55e78f76efb622495febdc1e097297505aab
Kate Howard <k.l.howard.1992@gmail.com>
2019-10-03 11:21:59 +0100

    Updates

---
+++ precache-manifest.36957feb5085ff2c518eb8fe1e1ad385.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "460957134cf619e94df2086a0501b05b",
+    "url": "/snek/index.html"
+  },
+  {
+    "revision": "548e1e8fae9e91c1f67a",
+    "url": "/snek/static/css/main.4a5bc3f6.chunk.css"
+  },
+  {
+    "revision": "440f700242cbf60c7f38",
+    "url": "/snek/static/js/2.c16ed238.chunk.js"
+  },
+  {
+    "revision": "548e1e8fae9e91c1f67a",
+    "url": "/snek/static/js/main.0617f1e9.chunk.js"
+  },
+  {
+    "revision": "d779b0bd85aff5c41b77",
+    "url": "/snek/static/js/runtime-main.3dbc6c52.js"
+  }
+]);
