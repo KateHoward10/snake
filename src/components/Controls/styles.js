17190b605446ab3805631228e7be6a8072992e2c
Kate Howard <k.l.howard.1992@gmail.com>
2020-04-28 13:25:40 +0100

    Adjust cookie warning and mobile styling

--- src/components/Controls/styles.js
+++ src/components/Controls/styles.js
@@ -1,5 +1,15 @@
 import styled from 'styled-components';
 
+const defaults = `
+  margin: 10px;
+	border: 2px solid orange;
+	font-size: 3vw;
+	@media screen and (min-width: 600px) {
+		font-size: 18px;
+    border-width: 4px;
+	}
+`;
+
 export const Container = styled.div`
 	display: flex;
 	flex-direction: row;
@@ -8,41 +18,27 @@ export const Container = styled.div`
 `;
 
 export const Button = styled.button`
-	margin: 10px;
 	padding: 5px;
 	background-color: orange;
-	border: 4px solid orange;
 	border-radius: 10px;
-	font-size: 3vw;
-	@media screen and (min-width: 600px) {
-		font-size: 18px;
-	}
+  font-size: 3vw;
+  ${defaults}
 `;
 
 export const QuestionButton = styled.button`
 	width: 30px;
 	height: 30px;
-	margin: 10px;
 	display: flex;
 	align-items: center;
 	justify-content: center;
 	background-color: orange;
-	border: 4px solid orange;
 	border-radius: 50%;
-	font-size: 3vw;
-	@media screen and (min-width: 600px) {
-		font-size: 18px;
-	}
+  ${defaults}
 `;
 
 export const Score = styled.p`
-	margin: 10px;
 	padding: 5px;
 	background-color: transparent;
-	border: 4px solid orange;
 	border-radius: 10px;
-	font-size: 3vw;
-	@media screen and (min-width: 600px) {
-		font-size: 18px;
-	}
+  ${defaults}
 `;
--- src/components/Cookies/index.js
+++ src/components/Cookies/index.js
@@ -5,8 +5,10 @@ function Cookies({ onAccept, onReject }) {
   return (
     <Container>
       <Text>This game uses cookies to save your high score, but can function without them. Is this OK with you?</Text>
-      <Button onClick={onReject}>No thanks</Button>
-      <Button onClick={onAccept}>Yep, no problem</Button>
+      <div>
+        <Button onClick={onReject}>No thanks</Button>
+        <Button onClick={onAccept}>Yep, no problem</Button>
+      </div>
     </Container>
   );
 }
