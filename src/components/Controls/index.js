ab5107b46d63ee79c6aadbd6ec55abcb50cf712a
Kate Howard <k.l.howard.1992@gmail.com>
2020-05-14 12:30:58 +0100

    Styling for hovered buttons

--- src/components/Controls/index.js
+++ src/components/Controls/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Container, Button, QuestionButton, Score } from './styles';
 
-function Controls({ onClick, score, highScore, openInstructions }) {
-  return (
-    <Container>
-      <Button onClick={onClick}>PLAY</Button>
-      <Score>Score: {score}</Score>
-      <Score>High Score: {highScore}</Score>
-      <QuestionButton onClick={openInstructions}>?</QuestionButton>
-    </Container>
-  );
-}
+const Controls = ({ onClick, score, highScore, openInstructions }) => (
+  <Container>
+    <Button onClick={onClick}>PLAY</Button>
+    <Score>Score: {score}</Score>
+    <Score>High Score: {highScore}</Score>
+    <QuestionButton onClick={openInstructions}>?</QuestionButton>
+  </Container>
+);
 
 export default Controls;
